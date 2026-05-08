4e4c526eff26ae4377c6ba8dade1ed6ac54654bc
Anthony Lieuallen <arantius@gmail.com>
2026-05-08 10:24:05 -0400

    Implement the popup menu.
    Just about a month ago:
    https://developer.mozilla.org/en-US/docs/Mozilla/Firefox/Releases/149
    "A user gesture is no longer required for ... browserAction.openPopup to open a popup."
    
    So the Alt-Down hotkey I always wanted (and which always was a user gesture!) will work now.
    
    EXCEPT I'm foiled again:
    https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/browser_action
    "If you supply a popup, then the popup is opened when the user clicks the button, and your JavaScript running in the popup can handle the user's interaction with it. If you don't supply a popup, then a click event is dispatched to your extension's background scripts when the user clicks the button."
    We can have a popup OR we can handle clicks, but we can't do both!
    
    (So we'll have to do something more, like awkwardly opening our own non-popup window?)
    
    References #9

--- popup.js
+++ popup.js
@@ -1,19 +1,62 @@
 let list = document.querySelector('ul');
+let currentTab = null;
+
+
+function go(event) {
+  const focusedEl = document.activeElement;
+  if (!focusedEl) return;
+  let url = focusedEl.textContent;
+  browser.tabs.update({'url': url});
+  window.close();
+}
+
+function handleArrowKey(event) {
+  const focusedEl = document.activeElement;
+  if (focusedEl && event.key == 'Enter') {
+    go();
+    return;
+  }
+  if (event.key != 'ArrowDown' && event.key != 'ArrowUp') return;
+
+  const listItems = Array.from(list.children);
+  event.preventDefault(); // Prevent default scroll behavior
+
+  let nextIndex = -1;
+  if (!focusedEl) {
+    nextIndex = 0;
+  } else {
+    const currentIndex = listItems.indexOf(focusedEl);
+
+    if (event.key === 'ArrowDown') {
+      nextIndex = (currentIndex + 1) % listItems.length;
+    } else if (event.key === 'ArrowUp') {
+      nextIndex = (currentIndex - 1 + listItems.length) % listItems.length;
+    }
+  }
+
+  if (nextIndex !== -1) {
+    listItems[nextIndex].focus();
+  }
+}
+
 
 browser.tabs.query({'active': true}).then(tabs => {
-  let currentTab = tabs.length && tabs[0] || null;
+  currentTab = tabs.length && tabs[0] || null;
   if (!currentTab) {
     console.warn('No current tab?!');
     return;
   }
-  let currentUrl = currentTab.url;
 
-  let urls = getUrlsFor(currentUrl);
+  let urls = getUrlsFor(currentTab.url);
+  let i = 1;
   for (let url of urls.list) {
     let li = document.createElement('li');
     li.textContent = url;
+    li.tabIndex = i++;
     list.appendChild(li);
   }
 });
 
-// How to open this popup?!
+
+window.addEventListener('keydown', handleArrowKey);
+list.addEventListener('click', go);
